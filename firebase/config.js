36960d4ee90aa116ca485512cb3ffe9065b1517e
ArceSegura <segura.david@live.com.mx>
2021-01-12 20:28:59 -0600

    Finalizado

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcxyIw-M4mfQLz_hEVbZWIrlP1o0NpqJ8",
+    authDomain: "product-hunt-5c235.firebaseapp.com",
+    projectId: "product-hunt-5c235",
+    storageBucket: "product-hunt-5c235.appspot.com",
+    messagingSenderId: "939845879558",
+    appId: "1:939845879558:web:abec30d1accd3eb73b0087"
+};
+
+export default firebaseConfig
